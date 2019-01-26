b0addd3fcf1f4dffeb593a92f21d279c2169060c
ylt94 <857425891@qq.com>
2019-01-26 17:12:08 +0800

    update

--- src/test.go
+++ src/test.go
@@ -4,6 +4,7 @@ import (
 "net/http"
 "strings"
 "log"
+"./index"
 )
 func sayhelloName(w http.ResponseWriter, r *http.Request) {
     r.ParseForm() //解析参数，默认是不会解析的
@@ -18,22 +19,9 @@ func sayhelloName(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintf(w, "Hello astaxie!") //这个写入到w的是输出到客户端的
 }
 
-// func index(w http.ResponseWriter, r *http.Request) {
-//     r.ParseForm() //解析参数，默认是不会解析的
-//     fmt.Println(r.Form) //这些信息是输出到服务器端的打印信息
-//     fmt.Println("path", r.URL.Path)
-//     fmt.Println("scheme", r.URL.Scheme)
-//     fmt.Println(r.Form["url_long"])
-//     for k, v := range r.Form {
-//         fmt.Println("key:", k)
-//         fmt.Println("val:", strings.Join(v, ""))
-//     }
-//     fmt.Fprintf(w, "index") //这个写入到w的是输出到客户端的
-// }
-
 func main() {
     http.HandleFunc("/", sayhelloName) //设置访问的路由
-    //http.HandleFunc("/index", index.index) //设置访问的路由
+    http.HandleFunc("/index", index.Index) //设置访问的路由
     err := http.ListenAndServe(":9090", nil) //设置监听的端口
     if err != nil {
         log.Fatal("ListenAndServe: ", err)
